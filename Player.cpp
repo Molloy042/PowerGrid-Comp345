520cbb9ac6c2d43c9b5a1d1c50001b4cf313a1cd
TrevorLall <tlall0308@gmail.com>
2019-02-09 17:03:51 -0500

    Add files via upload

---
+++ Player.cpp
@@ -0,0 +1,37 @@
+/*
+ * Player.cpp
+ *
+ *  Created on: Feb 9, 2019
+ *      Author: t_lall
+ */
+
+#include "Player.h"
+using namespace std;
+
+Player::Player(){
+	name = "";
+	player_num = 0;
+	elektro = 0;
+	colour = "";
+}
+
+Player::Player(int p_num, string player_name, int player_money, string player_colour){
+	name = player_name;
+	player_num = p_num;
+	elektro = player_money;
+	colour = player_colour;
+}
+
+void Player::setElektro(int money_spent){
+	elektro = elektro - money_spent;
+}
+
+
+int main() {
+	Player c(1,"asdven", 0, "green");
+//	c.setPlayer("Marven");
+	cout << c.getPlayer();
+	return 0;
+}
+
+
